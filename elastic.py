0a659e743bff9316557ded9cdb72645e5176498f
w140601 <w140601@outlook.com>
2018-10-23 10:02:44 -0500

    add class

--- elastic.py
+++ elastic.py
@@ -171,118 +171,129 @@ def elastic_constant_3d(T0,HH,KK,LL,N):
 
   return SIGMA00,SIGMADEL0,omega,G,g,n,delta,epsilon00,lambda0,lambdadel,iGgsigma0
 
-def assigndeltac(nr,c,N):
-    deltack=tf.Variable(tf.zeros((N,N,N),dtype=tf.complex64))
-    deltacr=tf.Variable(tf.zeros((N,N,N),dtype=tf.complex64))
-    deltack2=tf.Variable(tf.zeros((N,N,N),dtype=tf.complex64))
-    assigndelt=tf.group(deltack.assign(tf.fft3d(nr-c)),
-                        deltacr.assign(nr-c),
-                        deltack2.assign(tf.fft3d((nr-c)**2)))
-    return assigndelt, deltack, deltacr, deltack2
 
-#iGgsigma0tf=tf.placeholder("complex64",shape=(3,N,N,N))
-def first_order_disp(iGgsigma0tf,deltack,N):
+class elastic_part_update:
 
-    # calculate first order approximation of displacement  A PHASE-FIELD MODEL FOR EVOLVING MICROSTRUCTURES WITH STRONG ELASTIC INHOMOGENEITY
-    v_0=tf.Variable(tf.zeros((3,N,N,N),dtype=tf.complex64))
-    v_0assignstep=tf.group(
-            v_0[0,:,:,:].assign(tf.squeeze(iGgsigma0tf[0,:,:,:])*deltack),
-            v_0[1,:,:,:].assign(tf.squeeze(iGgsigma0tf[1,:,:,:])*deltack),
-            v_0[2,:,:,:].assign(tf.squeeze(iGgsigma0tf[2,:,:,:])*deltack)
-            )
-    return v_0, v_0assignstep
+    def __init__(self,iGgsigma0,  G, SIGMA00 , SIGMADEL0, g ):
+        iGgsigma0tf: 
+        Gtf:
+        SIGMA00tf:
+        SIGMADEL0tf:
+        gtf:
 
-def higherorderdisp(v_0,Gtf,SIGMA00tf,SIGMADEL0tf,N,gtf,deltacr,lambdadel,deltack,deltack2):
-    du_0=tf.Variable(tf.zeros((3,3,N,N,N),dtype=tf.complex64))
-    for l in range(3):
-        for m in range(3):
-            with tf.control_dependencies([du_0[l,m,:,:,:].assign(tf.ifft3d(1j*tf.squeeze(v_0[l,:,:,:])*tf.squeeze(gtf[m,:,:,:]))),\
-                du_0[l,m,:,:,:].assign(tf.fft3d(deltacr*tf.squeeze(du_0[l,m,:,:,:])))]):
-                du_0=tf.identity(du_0)
-    
-    delcdu_0=tf.Variable(tf.zeros((6,N,N,N),dtype=tf.complex64))
-    with tf.control_dependencies(
-    [delcdu_0[0,:,:,:].assign(du_0[0,0,:,:,:]),
-    delcdu_0[1,:,:,:].assign(du_0[1,1,:,:,:]),
-    delcdu_0[2,:,:,:].assign(du_0[2,2,:,:,:]),
-    delcdu_0[3,:,:,:].assign(du_0[0,1,:,:,:]+du_0[1,0,:,:,:]),
-    delcdu_0[4,:,:,:].assign(du_0[0,2,:,:,:]+du_0[2,0,:,:,:]),
-    delcdu_0[5,:,:,:].assign(du_0[1,2,:,:,:]+du_0[2,1,:,:,:]),]):
-        delcdu_0=tf.identity(delcdu_0)
-    
-    sigma3=tf.reshape(tf.matmul(lambdadel,tf.reshape(delcdu_0,[6,-1])),[6,N,N,N]) # third term in V_k1(g) expression in square paracenes
-    
-    SIGMA3=tf.Variable(tf.zeros((3,3,N,N,N),dtype=tf.complex64))
-    with tf.control_dependencies(
-    [SIGMA3[0,0,:,:,:].assign(sigma3[0,:,:,:]),
-    SIGMA3[1,1,:,:,:].assign(sigma3[1,:,:,:]),
-    SIGMA3[2,2,:,:,:].assign(sigma3[2,:,:,:]),
-    SIGMA3[0,1,:,:,:].assign(sigma3[3,:,:,:]),
-    SIGMA3[1,0,:,:,:].assign(sigma3[3,:,:,:]),
-    SIGMA3[0,2,:,:,:].assign(sigma3[4,:,:,:]),
-    SIGMA3[2,0,:,:,:].assign(sigma3[4,:,:,:]),
-    SIGMA3[1,2,:,:,:].assign(sigma3[5,:,:,:]),
-    SIGMA3[2,1,:,:,:].assign(sigma3[5,:,:,:])]):
-        SIGMA3=tf.identity(SIGMA3)
 
-    SIGMAtotal=tf.Variable(tf.zeros((3,3,N,N,N),dtype=tf.complex64))
+    def assigndeltac(nr,c,N):
+        deltack=tf.Variable(tf.zeros((N,N,N),dtype=tf.complex64))
+        deltacr=tf.Variable(tf.zeros((N,N,N),dtype=tf.complex64))
+        deltack2=tf.Variable(tf.zeros((N,N,N),dtype=tf.complex64))
+        assigndelt=tf.group(deltack.assign(tf.fft3d(nr-c)),
+                            deltacr.assign(nr-c),
+                            deltack2.assign(tf.fft3d((nr-c)**2)))
+        return assigndelt, deltack, deltacr, deltack2
 
-    for l in range(3):
-        for m in range(3):
-            with tf.control_dependencies([SIGMAtotal[l,m,:,:,:].assign(tf.squeeze(SIGMA00tf[l,m,:,:,:])*deltack+squeeze(SIGMADEL0tf[l,m,:,:,:])*deltack2-tf.squeeze(SIGMA3[l,m,:,:,:]))]):
-                SIGMAtotal=tf.identity(SIGMAtotal)
-    
-       
-    gtfexp=tf.expand_dims(gtf,1)
-    u_1=-1j*tf.squeeze(tf.matmul(tf.transpose(Gtf,perm=[1,0,2,3,4]),tf.matmul(SIGMAtotal,gtfexp)))
-    
-    dif=tf.abs(u_1-v_0)
-    dif=tf.reduce_sum(dif)
-    assignup=v_0.assign(u_1)
-    return u_1,dif,assignup
+    #iGgsigma0tf=tf.placeholder("complex64",shape=(3,N,N,N))
+    def first_order_disp(iGgsigma0tf,deltack,N):
+
+        # calculate first order approximation of displacement  A PHASE-FIELD MODEL FOR EVOLVING MICROSTRUCTURES WITH STRONG ELASTIC INHOMOGENEITY
+        v_0=tf.Variable(tf.zeros((3,N,N,N),dtype=tf.complex64))
+        v_0assignstep=tf.group(
+                v_0[0,:,:,:].assign(tf.squeeze(iGgsigma0tf[0,:,:,:])*deltack),
+                v_0[1,:,:,:].assign(tf.squeeze(iGgsigma0tf[1,:,:,:])*deltack),
+                v_0[2,:,:,:].assign(tf.squeeze(iGgsigma0tf[2,:,:,:])*deltack)
+                )
+        return v_0, v_0assignstep
+
+    def higherorderdisp(v_0,Gtf,SIGMA00tf,SIGMADEL0tf,N,gtf,deltacr,lambdadel,deltack,deltack2):
+        du_0=tf.Variable(tf.zeros((3,3,N,N,N),dtype=tf.complex64))
+        for l in range(3):
+            for m in range(3):
+                with tf.control_dependencies([du_0[l,m,:,:,:].assign(tf.ifft3d(1j*tf.squeeze(v_0[l,:,:,:])*tf.squeeze(gtf[m,:,:,:]))),\
+                    du_0[l,m,:,:,:].assign(tf.fft3d(deltacr*tf.squeeze(du_0[l,m,:,:,:])))]):
+                    du_0=tf.identity(du_0)
         
-    
-
+        delcdu_0=tf.Variable(tf.zeros((6,N,N,N),dtype=tf.complex64))
+        with tf.control_dependencies(
+        [delcdu_0[0,:,:,:].assign(du_0[0,0,:,:,:]),
+        delcdu_0[1,:,:,:].assign(du_0[1,1,:,:,:]),
+        delcdu_0[2,:,:,:].assign(du_0[2,2,:,:,:]),
+        delcdu_0[3,:,:,:].assign(du_0[0,1,:,:,:]+du_0[1,0,:,:,:]),
+        delcdu_0[4,:,:,:].assign(du_0[0,2,:,:,:]+du_0[2,0,:,:,:]),
+        delcdu_0[5,:,:,:].assign(du_0[1,2,:,:,:]+du_0[2,1,:,:,:]),]):
+            delcdu_0=tf.identity(delcdu_0)
         
-    
-
-
-def high_order_disp_assign_uel(u_1,v_0,dif,assignup,sess,gtf,N):  
-  
-    for i in range(100):
-        difval=sess.run(dif,feed_dict={iGgsigma0tf: iGgsigma0, Gtf: G, SIGMA00tf:SIGMA00 , SIGMADEL0tf:SIGMADEL0, gtf:g })
-        if np.abs(difval)<0.0001:
-            break
-        else:
-            sess.run(assignup)
-  
-  # Calculate delta epsilon
-    deltaep=tf.Variable(tf.zeros((3,3,N,N,N),dtype=tf.complex64)) # deltaepsilon(r) in real space
-    gtf=tf.placeholder("complex64",shape=(3,N,N,N))
-  
-    for i in range(3):
-        for j in range(3):
-            with tf.control_dependencies([deltaep[i,j,:,:,:].assign(tf.ifft3d(tf.squeeze(1j/2*tf.squeeze(u_1[i,:,:,:]*gtf[j,:,:,:]+u_1[j,:,:,:]*gtf[i,:,:,:]))))]):
-                deltaep=tf.identity(deltaep)
+        sigma3=tf.reshape(tf.matmul(lambdadel,tf.reshape(delcdu_0,[6,-1])),[6,N,N,N]) # third term in V_k1(g) expression in square paracenes
         
-    deltaep_0=tf.Variable(tf.zeros((6,N,N,N),dtype=tf.complex64))
-    with tf.control_dependencies(
-    [deltaep_0[0,:,:,:].assign(deltaep[0,0,:,:,:]),
-    deltaep_0[1,:,:,:].assign(deltaep[1,1,:,:,:]),
-    deltaep_0[2,:,:,:].assign(deltaep[2,2,:,:,:]),
-    deltaep_0[3,:,:,:].assign(2*deltaep[0,1,:,:,:]),
-    deltaep_0[4,:,:,:].assign(2*deltaep[0,2,:,:,:]),
-    deltaep_0[5,:,:,:].assign(2*deltaep[1,2,:,:,:])]):
-        deltaep_0=tf.identity(deltaep_0)
-    
-    SIGMADELDEL=tf.reshape(tf.matmul(lambdadel,tf.reshape(deltaep_0,[6,-1])),[6,N,N,N])
-    
-    uel=tf.fft3d(-tf.reduce_sum(SIGMA00*deltaep,[0,1])+tf.reduce_sum(SIGMA00*epsilon00,[0,1])*deltacr+1.0/2*tf.reduce_sum(SIGMADELDEL*deltaep_0,[0])\
-    -2*tf.reduce_sum(SIGMADEL0*deltaep,[0,1])*deltacr+3/2*tf.reduce_sum(SIGMADEL0*epsilon00,[0,1])*deltacr**2)
+        SIGMA3=tf.Variable(tf.zeros((3,3,N,N,N),dtype=tf.complex64))
+        with tf.control_dependencies(
+        [SIGMA3[0,0,:,:,:].assign(sigma3[0,:,:,:]),
+        SIGMA3[1,1,:,:,:].assign(sigma3[1,:,:,:]),
+        SIGMA3[2,2,:,:,:].assign(sigma3[2,:,:,:]),
+        SIGMA3[0,1,:,:,:].assign(sigma3[3,:,:,:]),
+        SIGMA3[1,0,:,:,:].assign(sigma3[3,:,:,:]),
+        SIGMA3[0,2,:,:,:].assign(sigma3[4,:,:,:]),
+        SIGMA3[2,0,:,:,:].assign(sigma3[4,:,:,:]),
+        SIGMA3[1,2,:,:,:].assign(sigma3[5,:,:,:]),
+        SIGMA3[2,1,:,:,:].assign(sigma3[5,:,:,:])]):
+            SIGMA3=tf.identity(SIGMA3)
 
-    return uel
+        SIGMAtotal=tf.Variable(tf.zeros((3,3,N,N,N),dtype=tf.complex64))
 
-  
+        for l in range(3):
+            for m in range(3):
+                with tf.control_dependencies([SIGMAtotal[l,m,:,:,:].assign(tf.squeeze(SIGMA00tf[l,m,:,:,:])*deltack+squeeze(SIGMADEL0tf[l,m,:,:,:])*deltack2-tf.squeeze(SIGMA3[l,m,:,:,:]))]):
+                    SIGMAtotal=tf.identity(SIGMAtotal)
+        
+        
+        gtfexp=tf.expand_dims(gtf,1)
+        u_1=-1j*tf.squeeze(tf.matmul(tf.transpose(Gtf,perm=[1,0,2,3,4]),tf.matmul(SIGMAtotal,gtfexp)))
+        
+        dif=tf.abs(u_1-v_0)
+        dif=tf.reduce_sum(dif)
+        assignup=v_0.assign(u_1)
+        return u_1,dif,assignup
+            
+        
+
+            
+        
+
+
+    def high_order_disp_assign_uel(u_1,v_0,dif,assignup,sess,gtf,N):  
+    
+        for i in range(100):
+            difval=sess.run(dif,feed_dict={iGgsigma0tf: iGgsigma0, Gtf: G, SIGMA00tf:SIGMA00 , SIGMADEL0tf:SIGMADEL0, gtf:g })
+            if np.abs(difval)<0.0001:
+                break
+            else:
+                sess.run(assignup)
+    
+    # Calculate delta epsilon
+        deltaep=tf.Variable(tf.zeros((3,3,N,N,N),dtype=tf.complex64)) # deltaepsilon(r) in real space
+        gtf=tf.placeholder("complex64",shape=(3,N,N,N))
+    
+        for i in range(3):
+            for j in range(3):
+                with tf.control_dependencies([deltaep[i,j,:,:,:].assign(tf.ifft3d(tf.squeeze(1j/2*tf.squeeze(u_1[i,:,:,:]*gtf[j,:,:,:]+u_1[j,:,:,:]*gtf[i,:,:,:]))))]):
+                    deltaep=tf.identity(deltaep)
+            
+        deltaep_0=tf.Variable(tf.zeros((6,N,N,N),dtype=tf.complex64))
+        with tf.control_dependencies(
+        [deltaep_0[0,:,:,:].assign(deltaep[0,0,:,:,:]),
+        deltaep_0[1,:,:,:].assign(deltaep[1,1,:,:,:]),
+        deltaep_0[2,:,:,:].assign(deltaep[2,2,:,:,:]),
+        deltaep_0[3,:,:,:].assign(2*deltaep[0,1,:,:,:]),
+        deltaep_0[4,:,:,:].assign(2*deltaep[0,2,:,:,:]),
+        deltaep_0[5,:,:,:].assign(2*deltaep[1,2,:,:,:])]):
+            deltaep_0=tf.identity(deltaep_0)
+        
+        SIGMADELDEL=tf.reshape(tf.matmul(lambdadel,tf.reshape(deltaep_0,[6,-1])),[6,N,N,N])
+        
+        uel=tf.fft3d(-tf.reduce_sum(SIGMA00*deltaep,[0,1])+tf.reduce_sum(SIGMA00*epsilon00,[0,1])*deltacr+1.0/2*tf.reduce_sum(SIGMADELDEL*deltaep_0,[0])\
+        -2*tf.reduce_sum(SIGMADEL0*deltaep,[0,1])*deltacr+3/2*tf.reduce_sum(SIGMADEL0*epsilon00,[0,1])*deltacr**2)
+
+        return uel
+
+    
 
   
 
